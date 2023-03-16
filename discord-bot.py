cb0d7115be0a519853e093ff81a72e17d2834a89
JimKwikX <78541304+JimSolomon@users.noreply.github.com>
2023-03-16 19:06:24 +0000

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 
-
 import nest_asyncio
-nest_asyncio.apply()
 import os
 import discord
 from discord.ext import commands
@@ -11,9 +9,14 @@ import openai
 import asyncio
 
 
+nest_asyncio.apply()
 
+async def time_consuming_operation():
+    # Perform time-consuming operation here
+    await asyncio.sleep(10)
 
-os.environ['OPENAI_API_KEY'] = 'API-KEY'
+os.environ['OPENAI_API_KEY'] = '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'
+# openai.api_base = "https://api.openai.com/v1/chat"  # Specify the API endpoint
 
 
 # Set up the Discord bot
@@ -29,6 +32,7 @@ async def my_coroutine(message):
     # Ignore messages sent by the bot itself
     if message.author == bot.user:
         return
+    task = asyncio.create_task(time_consuming_operation())
 
     # Generate a response using OpenAI API
     response = openai.Completion.create(
@@ -39,13 +43,21 @@ async def my_coroutine(message):
     )
 
     # Send the response back to the user
+    print("Doing other things while time-consuming operation runs...")
     await message.channel.send(response.choices[0].text)
+    await task
+    print("Time-consuming operation is complete.")
 
 # Define an event handler to call the coroutine when a message is received
 @bot.event
 async def on_message(message):
     await my_coroutine(message)
 
+bot.run('MTA4NTMxOTM4MjExNDIzODU5NQ.GIiaqM.MBvdP2kftSrL977r3yRVTM99ozbJkNX_iQ4aoY')
+loop = asyncio.get_event_loop()
+loop.run_until_complete(my_coroutine())
 
 
-bot.run('BOT-TOKEN')
+
+
+
