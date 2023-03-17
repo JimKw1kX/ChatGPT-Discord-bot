02aa95137aca812974e21b1e0c6ea9f01a301c5a
JimKwikX <78541304+JimSolomon@users.noreply.github.com>
2023-03-17 10:38:54 +0000

    Update keep-colab-alive.js

--- keep-colab-alive.js
+++ keep-colab-alive.js
@@ -3,3 +3,5 @@ function ConnectButton(){
     document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click() 
 }
 setInterval(ConnectButton,60000);
+
+#https://colab.research.google.com/drive/1LPiMkWQNQcj7X61d62NkgNZdg_rZiVJr#scrollTo=TE5_izRlYtrP
