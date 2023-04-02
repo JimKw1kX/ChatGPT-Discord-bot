26959e2d3a5dfab91bb2b9b895d63eb9fd2e19f0
JimKwikX <78541304+JimSolomon@users.noreply.github.com>
2023-04-02 09:48:58 +0100

    Delete keep-colab-alive.js

--- keep-colab-alive.js
+++
@@ -1,8 +0,0 @@
-function ConnectButton(){
-    console.log("Connect pushed"); 
-    document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click() 
-}
-setInterval(ConnectButton,60000);
-
-//https://colab.research.google.com/drive/1LPiMkWQNQcj7X61d62NkgNZdg_rZiVJr#scrollTo=TE5_izRlYtrP
-//https://stackoverflow.com/questions/71456390/how-to-keep-the-google-colab-running-without-disconnecting-in-2022
